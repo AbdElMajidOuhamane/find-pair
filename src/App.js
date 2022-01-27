8e7ac0e28ffead39b08168ace8325e948758f201
majid <abdelmajidouhamane01@gmail.com>
2022-01-27 23:09:06 +0100

    i fixed something ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,150 @@
-import logo from './logo.svg';
-import './App.css';
+import {Container,
+  CardFlip,
+  Score,
+  UnknowCard,
+  SecInfo,
+  Title,ScoreBar,
+  TitleScore,
+  Paragraph,
+  OptionScore,
+  Size,
+  ButtonRestart,
+  TotalScore,
+Span,Line,DropDown,OptionPair,Pt,OptionSize} from "./components/styles/Container.style";
+import {useState} from "react"
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Title>Find Pair</Title>
+    <Container>
+      <SecInfo>
+      <CardFlip>
+        <UnknowCard>
+          ?
+        </UnknowCard>
+   
+      </CardFlip>
+      <CardFlip>
+        <UnknowCard>
+          ?
+        </UnknowCard>
+   
+      </CardFlip>
+      <CardFlip>
+        <UnknowCard>
+          ?
+        </UnknowCard>
+   
+      </CardFlip>
+      <CardFlip>
+        <UnknowCard>
+          ?
+        </UnknowCard>
+   
+      </CardFlip>
+      <CardFlip>
+        <UnknowCard>
+          ?
+        </UnknowCard>
+   
+      </CardFlip>
+      <CardFlip>
+        <UnknowCard>
+          ?
+        </UnknowCard>
+   
+      </CardFlip>
+      <CardFlip>
+        <UnknowCard>
+          ?
+        </UnknowCard>
+   
+      </CardFlip>
+      <CardFlip>
+        <UnknowCard>
+          ?
+        </UnknowCard>
+   
+      </CardFlip>
+      <CardFlip>
+        <UnknowCard>
+          ?
+        </UnknowCard>
+   
+      </CardFlip>
+      <CardFlip>
+        <UnknowCard>
+          ?
+        </UnknowCard>
+   
+      </CardFlip>
+      <CardFlip>
+        <UnknowCard>
+          ?
+        </UnknowCard>
+   
+      </CardFlip>
+      <CardFlip>
+        <UnknowCard>
+          ?
+        </UnknowCard>
+   
+      </CardFlip>
+      <CardFlip>
+        <UnknowCard>
+          ?
+        </UnknowCard>
+   
+      </CardFlip>
+      <CardFlip>
+        <UnknowCard>
+          ?
+        </UnknowCard>
+   
+      </CardFlip>
+      <CardFlip>
+        <UnknowCard>
+          ?
+        </UnknowCard>
+   
+      </CardFlip>
+      <CardFlip>
+        <UnknowCard>
+          ?
+        </UnknowCard>
+   
+      </CardFlip>
+   
+    </SecInfo>
+    <Score>
+      <ScoreBar>
+        <TitleScore>Score</TitleScore>
+        <TotalScore><Span color="#1890FF">2</Span> / 10</TotalScore>
+        <Paragraph>Tries: 5</Paragraph>
+      </ScoreBar>
+
+      <Line />
+
+      <OptionScore>
+        <TitleScore>Options</TitleScore>
+           <OptionSize>
+            <Size>Size</Size>
+            <DropDown>
+              <OptionPair>10 pairs</OptionPair>
+              <OptionPair>15 pairs</OptionPair>
+              </DropDown>
+              </OptionSize>
+              <ButtonRestart>
+                <Pt>Restart</Pt>
+              </ButtonRestart>
+      </OptionScore>
+    </Score>
+    </Container>
+
+   
     </div>
   );
 }
---
+++ src/components/styles/Container.style.js
@@ -0,0 +1,174 @@
+import styled from "styled-components"
+
+export const Title=styled.h1`
+display:flex;
+justify-content:center;
+align-items:center;
+`
+
+
+export const Container=styled.div`
+display: flex;
+background-color:#F2F2F2;
+margin-top:57px;
+flex-direction:row;
+justify-content:space-between;
+margin: 57px 178px 88px 178px;
+padding:20px;
+`
+export const SecInfo=styled.div`
+display: flex;
+flex-flow:row wrap;
+justify-content:space-around;
+background-color:#F2F2F2;
+height: 644px;
+width: 765px;
+
+`
+
+export const CardFlip=styled.button`
+display:flex;
+justify-content:center;
+align-items:center;
+height: 146px;
+width: 137px;
+border: none;
+background-color:#1890FF ;
+border-radius:4px;
+`
+export const UnknowCard=styled.p`
+text-align:center;
+font-family: Nunito;
+font-style: normal;
+font-weight: bold;
+font-size: 50px;
+color: #FFFFFF;
+`
+
+
+export const Score=styled.div`
+background-color:#FBFBFB ;
+width: 262px;
+height: 383px;
+margin-left:57px;
+
+
+`
+
+export const ScoreBar=styled.div`
+margin:11px 144px 29px 15px;
+width: 103px;
+height: 136px;
+
+`
+
+export const TitleScore=styled.h1`
+font-family: Nunito;
+font-style: normal;
+font-weight: bold;
+font-size: 20px;
+line-height: 27px;
+color: #232527;
+`
+export const TotalScore=styled.p`
+
+font-family: Nunito;
+font-style: normal;
+font-weight: bold;
+font-size: 35px;
+line-height: 48px;
+color: #232527;
+margin-top:20px;
+margin-bottom:19px;
+`
+
+export const Span=styled.span`
+color: #1890FF;
+
+`
+
+export const Paragraph=styled.p`
+font-family: OpenSans;
+font-size: 16px;
+line-height: 19px;
+margin:17px 49px 0 0;
+color: #232527;
+
+`
+export const Line=styled.div`
+
+border: 1px solid #B2B2B2;
+margin:30px 125px 30px 15px;
+
+`
+
+
+export const OptionScore=styled.div`
+margin:-7px 27px 13px 15px;
+position: absolute;
+width: 220px;
+height: 152px;
+`
+
+export const OptionSize=styled.div`
+
+display: flex;
+flex-direction:row;
+margin-top:30px;
+`
+
+
+export const Size=styled.label`
+font-family: Open Sans;
+font-size: 16px;
+line-height: 19px;
+color: #232527;
+height: 22px;
+margin-top:7px;
+`
+
+export const ButtonRestart=styled.button`
+display:flex;
+justify-content:center;
+align-items:center;
+position: absolute;
+width: 74px;
+height:32px;
+margin-top:20px;
+background: #1890FF;
+border:none;
+border-radius: 2px;
+
+
+`
+
+export const DropDown=styled.select`
+background: #FFFFFF;
+border: 1px solid rgba(0, 0, 0, 0.15);
+box-sizing: border-box;
+border-radius: 2px;
+margin-right:37px;
+margin-left:20px;
+width: 160px;
+height: 32px;
+margin-bottom:12px;
+color: #000000A6;
+`
+
+
+
+export const OptionPair=styled.option`
+font-family: Open Sans;
+font-style: normal;
+font-weight: normal;
+font-size: 14px;
+line-height: 22px;
+color: #000000A6;
+padding:5px 96px 5px 12px;
+`
+export const Pt=styled.p`
+font-family: Open Sans;
+color: #FFFFFF;
+
+
+`
